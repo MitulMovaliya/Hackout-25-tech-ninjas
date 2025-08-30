233b521de4c012b607010ad83751d903eb9ee6d5
Darsh_Balar <darshbalar2005@gmail.com>
2025-08-30 08:58:15 +0530

    Login and SignUp Page Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./components/Login";
+import SignupPage from "./components/SignUp";
+import Home from "./components/Home";
+
+const router = createBrowserRouter([
+  
+  {
+    path: "/",
+    element:
+      <Home/>
+  },
+  {
+    path: "/login",
+    element:
+      <LoginPage/>
+  },
+  {
+    path: "/signup",
+    element:
+      <SignupPage/>
+  }
+]);
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="h-screen">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
